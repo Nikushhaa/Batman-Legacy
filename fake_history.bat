af65687c65f0a233925e53f2295b1e26ba265535
Nikusha <nevermissacc@gmail.com>
2025-12-28 12:01:00 +0400

    update 1

--- fake_history.bat
+++ fake_history.bat
@@ -2,7 +2,7 @@
 
 set file=index.html
 
-for /L %%i in (1,1,31) do (
+for /L %%i in (1,1,21) do (
     if %%i lss 5 (
         echo ^<h1^>Update %%i^</h1^> >> %file%
     ) else if %%i lss 10 (
@@ -13,7 +13,7 @@ for /L %%i in (1,1,31) do (
 
     git add .
 
-    git commit --date="2026-02-22T12:%%i:00" -m "update %%i"
+    git commit --date="2025-12-28T12:%%i:00" -m "update %%i"
 )
 
 git push origin main
