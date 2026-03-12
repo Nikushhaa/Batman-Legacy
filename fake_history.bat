5cc05e49b54da428a2ba70cae7df7df459b4ba08
Nikusha <nevermissacc@gmail.com>
2026-03-12 12:01:25 +0400

    update 1

--- fake_history.bat
+++ fake_history.bat
@@ -13,7 +13,7 @@ for /L %%i in (1,1,21) do (
 
     git add .
 
-    git commit --date="2026-05-18T12:%%i:25" -m "update %%i"
+    git commit --date="2026-03-12T12:%%i:25" -m "update %%i"
 )
 
 git push origin main
