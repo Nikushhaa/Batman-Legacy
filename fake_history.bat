d67dba4b134f7f66b8fca4beb6086ea4e015a060
Nikusha <nevermissacc@gmail.com>
2026-04-10 12:39:00 +0400

    update 39

---
+++ fake_history.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set file=index.html
+
+for /L %%i in (1,1,19) do (
+    if %%i lss 5 (
+        echo ^<h1^>Update %%i^</h1^> >> %file%
+    ) else if %%i lss 10 (
+        echo ^<h2^>Update %%i^</h2^> >> %file%
+    ) else (
+        echo ^<p^>Update %%i^</p^> >> %file%
+    )
+
+    git add .
+
+    git commit --date="2026-04-10T12:%%i:00" -m "update %%i"
+)
+
+git push origin main
+
+pause
