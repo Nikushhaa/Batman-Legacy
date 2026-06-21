1819e63d10aed8d56a0a2f93fbc2275889139f29
Nikusha <nevermissacc@gmail.com>
2026-06-21 12:01:00 +0400

    update 1

--- fake_history.bat
+++ fake_history.bat
@@ -3,11 +3,17 @@
 set file=test.html
 
 for /L %%i in (1,1,15) do (
-    echo Update %%i >> %file%
+    if %%i lss 5 (
+        echo ^<h1^>Update %%i^</h1^> >> %file%
+    ) else if %%i lss 10 (
+        echo ^<h2^>Update %%i^</h2^> >> %file%
+    ) else (
+        echo ^<p^>Update %%i^</p^> >> %file%
+    )
 
     git add .
 
-    git commit --date="2026-06-21T12:%%i:00" -m "<p>update</> %%i"
+    git commit --date="2026-06-21T12:%%i:00" -m "update %%i"
 )
 
 git push origin main
